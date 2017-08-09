951118664ac5320f3b5e0bde25a8442898fd3fce
dixanta <dixanta@gmail.com>
2017-08-09 11:30:46 +0545

    Project Setup

---
+++ ParentProject/core-module/src/main/java/com/leapfrog/core/dao/ComplainDAO.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.leapfrog.core.dao;
+
+import com.leapfrog.core.entity.Complain;
+
+/**
+ *
+ * @author USER
+ */
+public interface ComplainDAO extends GenericDAO<Complain> {
+    
+}
